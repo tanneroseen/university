161ff4cc865c5fe1289d61a3ba47de2f7662fed3
tanneroseen <tanneroseen@gmail.com>
2022-11-30 21:23:24 -0700

    Update

--- test.py
+++ test.py
@@ -28,12 +28,6 @@ fancy_page_stuff = """
 <style>
 [data-testid="stAppViewContainer"] {
     background-image: url("https://cdn.getyourguide.com/img/location/5c9392236feff.jpeg/99.jpg");
-
-    filter: blur(5px)
-}
-
-[class="block-container css-12oz5g7 egzxvld2"] {
-    filter: blur(0px)
 }
 </style>
 """
